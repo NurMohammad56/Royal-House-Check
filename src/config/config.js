71b4d56c68f93beecde167c79706913bbf8bd03d
NurMohammad56 <milonsardar56@gmail.com>
2025-04-03 17:39:26 +0600

    feat: Update environment variables and enhance user authentication flow with token generation and logout functionality

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const MONGODB_URL = process.env.MONGODB_URL
+export const MONGODB_URI = process.env.MONGODB_URI
 
 export const PORT = Number(process.env.PORT)
 
