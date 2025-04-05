e5025ad780cdd4467bcb8710fdf35d5eb886436d
NavidulHoque <hnavidul@gmail.com>
2025-04-05 16:37:05 +0600

    feat(middleware): update role middleware to include client and staff roles

--- src/middleware/role.middleware.js
+++ src/middleware/role.middleware.js
@@ -3,15 +3,23 @@ export const isAdmin = (req, res, next) => {
     if (req.user && req.user.role === 'admin') {
         return next();
     }
-    
+
     return res.status(403).json({ status: false, message: 'Access denied. Admins only' });
 };
 
 
-export const isUser = (req, res, next) => {
-    if (req.user && req.user.role === 'user') {
+export const isClient = (req, res, next) => {
+    if (req.user && req.user.role === 'client') {
         return next();
     }
 
-    return res.status(403).json({ status: false, message: 'Access denied. Users only' });
+    return res.status(403).json({ status: false, message: 'Access denied. Clients only' });
+}
+
+export const isStaff = (req, res, next) => {
+    if (req.user && req.user.role === 'staff') {
+        return next();
+    }
+
+    return res.status(403).json({ status: false, message: 'Access denied. Staffs only' });
 }
