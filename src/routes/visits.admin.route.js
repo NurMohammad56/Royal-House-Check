7f3835df4e2de4122b9b9e29b90b992386c8c4f8
NavidulHoque <hnavidul@gmail.com>
2025-04-07 16:04:48 +0600

    feat(visits): add validation for visit creation and enforce future date requirement fix(visits): update route to require admin role for visit management endpoints

--- src/routes/visits.admin.route.js
+++ src/routes/visits.admin.route.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { createVisit, getAllVisitsCount, getCancelledVisits, getCompletedVisits, getConfirmedVisits, getPendingVisits, getPendingVisitsCount, updateVisit, updateVisitStaff } from "../controller/visits.admin.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { isAdmin } from "../middleware/role.middleware.js";
 
 const router = Router();
 
-router.post("/create-visit-admin", verifyJWT, createVisit);
-router.get('/get-all-visits-count', verifyJWT, getAllVisitsCount);
-router.get('/get-pending-visits-count', verifyJWT, getPendingVisitsCount)
-router.get('/get-confirmed-visits', verifyJWT, getConfirmedVisits);
-router.get('/get-pending-visits', verifyJWT, getPendingVisits);
-router.get('/get-successful-visits', verifyJWT, getCompletedVisits);
-router.get('/get-cancelled-visits', verifyJWT, getCancelledVisits)
-router.patch('/update-visit/:id', verifyJWT, updateVisit);
-router.patch('/update-visit-staff/:id', verifyJWT, updateVisitStaff);
+router.post("/create-visit", verifyJWT, isAdmin, createVisit);
+router.get('/get-all-visits-count', verifyJWT, isAdmin, getAllVisitsCount);
+router.get('/get-pending-visits-count', verifyJWT, isAdmin, getPendingVisitsCount)
+router.get('/get-confirmed-visits', verifyJWT, isAdmin, getConfirmedVisits);
+router.get('/get-pending-visits', verifyJWT, isAdmin, getPendingVisits);
+router.get('/get-successful-visits', verifyJWT, isAdmin, getCompletedVisits);
+router.get('/get-cancelled-visits', verifyJWT, isAdmin, getCancelledVisits)
+router.patch('/update-visit/:id', verifyJWT, isAdmin, updateVisit);
+router.patch('/update-visit-staff/:id', verifyJWT, isAdmin, updateVisitStaff);
 
 export default router;
