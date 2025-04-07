1e58ec525cbbd2f7df2c39a421497b30ca9600e1
NavidulHoque <hnavidul@gmail.com>
2025-04-07 16:44:38 +0600

    fix(routes): correct endpoint name for completed visits

--- src/routes/visits.admin.route.js
+++ src/routes/visits.admin.route.js
@@ -10,7 +10,7 @@ router.get('/get-all-visits-count', verifyJWT, isAdmin, getAllVisitsCount);
 router.get('/get-pending-visits-count', verifyJWT, isAdmin, getPendingVisitsCount)
 router.get('/get-confirmed-visits', verifyJWT, isAdmin, getConfirmedVisits);
 router.get('/get-pending-visits', verifyJWT, isAdmin, getPendingVisits);
-router.get('/get-successful-visits', verifyJWT, isAdmin, getCompletedVisits);
+router.get('/get-completed-visits', verifyJWT, isAdmin, getCompletedVisits);
 router.get('/get-cancelled-visits', verifyJWT, isAdmin, getCancelledVisits)
 router.patch('/update-visit/:id', verifyJWT, isAdmin, updateVisit);
 router.patch('/update-visit-staff/:id', verifyJWT, isAdmin, updateVisitStaff);
