5af893ff9d25c51f5d8ac13a1838b3f84026a2f1
NavidulHoque <hnavidul@gmail.com>
2025-04-08 13:23:07 +0600

    feat(visits): refactor visit handling and improve error responses

--- src/middleware/errorHandler.middleware.js
+++ src/middleware/errorHandler.middleware.js
@@ -3,6 +3,7 @@ const errorHandler = (error, req, res, next) => {
     console.error(error);
 
     return res.status(500).json({
+        status: false,
         message: error.message
     });
 };
--- src/services/visit.services.js
+++ src/services/visit.services.js
@@ -1,41 +1,74 @@
+import mongoose from "mongoose";
 import { Visit } from "../model/visit.model.js";
 import { generateCode } from "../utils/generateCode.js";
 
+export const createCode = async () => {
 
-export const createCode = async (next) => {
+    let code;
+    let visit
 
-    try {
-        let code;
-        let visit
+    do {
+        code = generateCode();
 
-        do {
-            code = generateCode();
+        visit = await Visit.findOne({ visitCode: code });
+    } while (visit)
 
-            visit = await Visit.findOne({ visitCode: code });
-        } while (visit)
-
-        return code;
-    }
-
-    catch (error) {
-        next(error)
-    }
+    return code;
 }
 
 //gets a specific visit
-export const getVisits = async (client, status, res, next) => {
+export const getVisits = async (client, status, res) => {
 
-    try {
-        const visits = await Visit.find({ client, status }).select("-createdAt -updatedAt -__v").sort({ date: 1 })
+    const visits = await Visit.find({ client, status }).select("-createdAt -updatedAt -__v").sort({ date: 1 })
 
-        return res.status(200).json({
-            status: true,
-            message: `${status} visits fetched successfully`,
-            data: visits
+    return res.status(200).json({
+        status: true,
+        message: `${status} visits fetched successfully`,
+        data: visits
+    })
+}
+
+export const updateVisitService = async (body, id, client, res) => {
+
+    const { date } = body
+
+    //checking if the provided date is in the future
+    if (new Date(date).getTime() < new Date().getTime()) {
+        return res.status(400).json({
+            status: false,
+            message: "Visit date must be in the future"
         })
     }
 
-    catch (error) {
-        next(error)
+    const visit = mongoose.Types.ObjectId.isValid(id) && await Visit.findById(id).select("status")
+
+    //checking if the visit id is valid or not
+    if (!visit) {
+        return res.status(404).json({
+            status: false,
+            message: "Visit not found"
+        });
     }
+
+    //checking if the visit is completed or cancelled
+    if (visit.status === "completed" || visit.status === "cancelled") {
+        return res.status(403).json({
+            status: false,
+            message: "You cannot update the visit which is completed or cancelled"
+        })
+    }
+
+    const existingVisit = await Visit.findOne({ date, client, $or: [{ status: "pending" }, { status: "confirmed" }] })
+
+    // checking if the visit date is already taken by another visit
+    if (existingVisit && existingVisit._id.toString() !== id) {
+        return res.status(400).json({
+            status: false,
+            message: "You already taken a visit in this date"
+        });
+    }
+
+    const updatedVisit = await Visit.findByIdAndUpdate(id, body, { new: true }).select("-createdAt -updatedAt -__v")
+
+    return updatedVisit
 }
